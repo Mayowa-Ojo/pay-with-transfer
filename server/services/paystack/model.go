93795ae5e6695dae012fbc99ec156f7002a205fd
Mayowa Ojo <46647640+Mayowa-Ojo@users.noreply.github.com>
2023-10-07 13:25:11 +0100

    implement paystack service

--- server/services/paystack/model.go
+++ server/services/paystack/model.go
@@ -1,8 +1,20 @@
 package paystack
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
 const (
 	ASSIGN_VIRTUAL_ACCOUNT_PATH = "/dedicated_account/assign"
 )
+
+const BANK_NAME_WEMA = "Wema Bank"
+const BANK_ID_WEMA = 20780
+const BANK_SLUG_WEMA = "wema-bank"
+
+const CURRENCY_NGN = "NGN"
 
 type AssignVirtualAccountRequest struct {
 	Email         string `json:"email" validate:"required"`
@@ -19,7 +31,75 @@ type AssignVirtualAccountRequest struct {
 	SplitCode     string `json:"split_code"`
 }
 
+type Customer struct {
+	ID                       int         `json:"id"`
+	FirstName                string      `json:"first_name"`
+	LastName                 string      `json:"last_name"`
+	Email                    string      `json:"email"`
+	CustomerCode             string      `json:"customer_code"`
+	Phone                    string      `json:"phone"`
+	Metadata                 interface{} `json:"metadata"`
+	RiskAction               string      `json:"risk_action"`
+	InternationalFormatPhone string      `json:"international_format_phone"`
+}
+type Bank struct {
+	Name string `json:"name"`
+	ID   int    `json:"id"`
+	Slug string `json:"slug"`
+}
+type Assignment struct {
+	Integration  int         `json:"integration"`
+	AssigneeID   int         `json:"assignee_id"`
+	AssigneeType string      `json:"assignee_type"`
+	Expired      bool        `json:"expired"`
+	AccountType  string      `json:"account_type"`
+	AssignedAt   time.Time   `json:"assigned_at"`
+	ExpiredAt    interface{} `json:"expired_at"`
+}
+type DedicatedAccount struct {
+	Bank          Bank        `json:"bank"`
+	AccountName   string      `json:"account_name"`
+	AccountNumber string      `json:"account_number"`
+	Assigned      bool        `json:"assigned"`
+	Currency      string      `json:"currency"`
+	Metadata      interface{} `json:"metadata"`
+	Active        bool        `json:"active"`
+	ID            int         `json:"id"`
+	CreatedAt     time.Time   `json:"created_at"`
+	UpdatedAt     time.Time   `json:"updated_at"`
+	Assignment    Assignment  `json:"assignment"`
+}
+type Identification struct {
+	Status string `json:"status"`
+}
+type Data struct {
+	Customer         Customer         `json:"customer"`
+	DedicatedAccount DedicatedAccount `json:"dedicated_account"`
+	Identification   Identification   `json:"identification"`
+}
 type AssignVirtualAccountResponse struct {
 	Status  bool   `json:"status"`
+	Data    Data   `json:"data"`
 	Message string `json:"message"`
+}
+
+func (r *AssignVirtualAccountResponse) WithAccountID() {
+	if r == nil {
+		return
+	}
+	r.Data.DedicatedAccount.ID, _ = strconv.Atoi(strconv.Itoa(int(time.Now().UnixMilli()))[7:])
+}
+
+func (r *AssignVirtualAccountResponse) WithCustomerID() {
+	if r == nil {
+		return
+	}
+	r.Data.Customer.ID, _ = strconv.Atoi(strconv.Itoa(int(time.Now().UnixMilli()))[7:])
+}
+
+func (r *AssignVirtualAccountResponse) WithCustomerCode() {
+	if r == nil {
+		return
+	}
+	r.Data.Customer.CustomerCode = fmt.Sprintf("cus-%s", strconv.Itoa(int(time.Now().UnixMilli()))[3:])
 }
--- server/services/paystack/service.go
+++ server/services/paystack/service.go
@@ -2,8 +2,11 @@ package paystack
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"pay-with-transfer/config"
+	"pay-with-transfer/shared"
+	"time"
 )
 
 type Service interface {
@@ -25,5 +28,30 @@ func New(cfg config.PaystackConfig, httpClient *http.Client) Service {
 }
 
 func (c *Client) CreateAndAssignVirtualAccount(ctx context.Context, req AssignVirtualAccountRequest) (*AssignVirtualAccountResponse, error) {
-	return nil, nil
+	resp := &AssignVirtualAccountResponse{}
+
+	resp.Status = true
+	resp.Message = "Assigned dedicated account"
+	resp.Data.Customer.FirstName = shared.DEFAULT_ACCOUNT_HOLDER_FIRST_NAME
+	resp.Data.Customer.LastName = shared.DEFAULT_ACCOUNT_HOLDER_LAST_NAME
+	resp.Data.Customer.Email = shared.GeneratePayEmail()
+	resp.Data.Customer.Phone = shared.GeneratePayPhoneNumber()
+	resp.Data.DedicatedAccount.Bank.Name = BANK_NAME_WEMA
+	resp.Data.DedicatedAccount.Bank.ID = BANK_ID_WEMA
+	resp.Data.DedicatedAccount.Bank.Slug = BANK_SLUG_WEMA
+	resp.Data.DedicatedAccount.AccountName = fmt.Sprintf("%s %s", shared.DEFAULT_ACCOUNT_HOLDER_FIRST_NAME, shared.DEFAULT_ACCOUNT_HOLDER_LAST_NAME)
+	resp.Data.DedicatedAccount.AccountNumber = shared.GenerateAccountNumber()
+	resp.Data.DedicatedAccount.Assigned = true
+	resp.Data.DedicatedAccount.Currency = CURRENCY_NGN
+	resp.Data.DedicatedAccount.Active = true
+	resp.Data.DedicatedAccount.CreatedAt = time.Now()
+	resp.Data.DedicatedAccount.UpdatedAt = time.Now()
+	resp.Data.Identification.Status = "success"
+	resp.WithCustomerID()
+	resp.WithCustomerCode()
+	resp.WithAccountID()
+
+	time.Sleep(time.Millisecond * 700)
+
+	return resp, nil
 }
