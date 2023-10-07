7fc3ed620baef5189a8355c77afdcdcaaff2a5bb
Mayowa Ojo <46647640+Mayowa-Ojo@users.noreply.github.com>
2023-10-07 13:24:52 +0100

    add tests

--- server/shared/common_test.go
+++ server/shared/common_test.go
@@ -1,6 +1,7 @@
 package shared
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,4 +13,37 @@ func TestBindReplacer(t *testing.T) {
 
 	got := BindReplacer(query)
 	assert.Equal(t, expected, got)
+}
+
+func TestToBaseUnitAmount(t *testing.T) {
+	testCases := []struct {
+		input    float64
+		expected int64
+	}{
+		{input: 1.87, expected: 187},
+		{input: 1.03, expected: 103},
+		{input: 0.01, expected: 1},
+		{input: 0.43, expected: 43},
+		{input: 1.15, expected: 115},
+		{input: 1.65, expected: 165},
+		{input: 200.03, expected: 20003},
+		{input: 15.33, expected: 1533},
+		{input: 2.60, expected: 260},
+		{input: 2.13, expected: 213},
+		{input: 2.98, expected: 298},
+		{input: 1.06, expected: 106},
+		{input: 300, expected: 30000},
+		{input: 518, expected: 51800},
+		{input: 0.14, expected: 14},
+		{input: 0.71, expected: 71},
+		{input: 0.33, expected: 33},
+		{input: 20755, expected: 2075500},
+	}
+
+	for idx, tc := range testCases {
+		t.Run(fmt.Sprintf("test-case-%d", idx), func(t *testing.T) {
+			resp := ToBaseUnitAmount(tc.input)
+			assert.Equal(t, tc.expected, resp)
+		})
+	}
 }
--- server/store/store_mock.go
+++ server/store/store_mock.go
@@ -34,6 +34,64 @@ func (m *MockStore) EXPECT() *MockStoreMockRecorder {
 	return m.recorder
 }
 
+// CreateAccountHolder mocks base method.
+func (m *MockStore) CreateAccountHolder(ctx context.Context, ah AccountHolder) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateAccountHolder", ctx, ah)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// CreateAccountHolder indicates an expected call of CreateAccountHolder.
+func (mr *MockStoreMockRecorder) CreateAccountHolder(ctx, ah interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateAccountHolder", reflect.TypeOf((*MockStore)(nil).CreateAccountHolder), ctx, ah)
+}
+
+// CreateEphemeralAccount mocks base method.
+func (m *MockStore) CreateEphemeralAccount(ctx context.Context, ea EphemeralAccount) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateEphemeralAccount", ctx, ea)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// CreateEphemeralAccount indicates an expected call of CreateEphemeralAccount.
+func (mr *MockStoreMockRecorder) CreateEphemeralAccount(ctx, ea interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateEphemeralAccount", reflect.TypeOf((*MockStore)(nil).CreateEphemeralAccount), ctx, ea)
+}
+
+// FindDormantAccount mocks base method.
+func (m *MockStore) FindDormantAccount(ctx context.Context) (*Account, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FindDormantAccount", ctx)
+	ret0, _ := ret[0].(*Account)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FindDormantAccount indicates an expected call of FindDormantAccount.
+func (mr *MockStoreMockRecorder) FindDormantAccount(ctx interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindDormantAccount", reflect.TypeOf((*MockStore)(nil).FindDormantAccount), ctx)
+}
+
+// FindEphemeralAccountByAccountID mocks base method.
+func (m *MockStore) FindEphemeralAccountByAccountID(ctx context.Context, accountID string) (*EphemeralAccount, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FindEphemeralAccountByAccountID", ctx, accountID)
+	ret0, _ := ret[0].(*EphemeralAccount)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FindEphemeralAccountByAccountID indicates an expected call of FindEphemeralAccountByAccountID.
+func (mr *MockStoreMockRecorder) FindEphemeralAccountByAccountID(ctx, accountID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindEphemeralAccountByAccountID", reflect.TypeOf((*MockStore)(nil).FindEphemeralAccountByAccountID), ctx, accountID)
+}
+
 // GetAccountByID mocks base method.
 func (m *MockStore) GetAccountByID(ctx context.Context, id string) (*Account, error) {
 	m.ctrl.T.Helper()
@@ -47,6 +105,20 @@ func (m *MockStore) GetAccountByID(ctx context.Context, id string) (*Account, er
 func (mr *MockStoreMockRecorder) GetAccountByID(ctx, id interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAccountByID", reflect.TypeOf((*MockStore)(nil).GetAccountByID), ctx, id)
+}
+
+// UpdateAccount mocks base method.
+func (m *MockStore) UpdateAccount(ctx context.Context, ac Account) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateAccount", ctx, ac)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateAccount indicates an expected call of UpdateAccount.
+func (mr *MockStoreMockRecorder) UpdateAccount(ctx, ac interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateAccount", reflect.TypeOf((*MockStore)(nil).UpdateAccount), ctx, ac)
 }
 
 // MockAccountStore is a mock of AccountStore interface.
@@ -72,6 +144,64 @@ func (m *MockAccountStore) EXPECT() *MockAccountStoreMockRecorder {
 	return m.recorder
 }
 
+// CreateAccountHolder mocks base method.
+func (m *MockAccountStore) CreateAccountHolder(ctx context.Context, ah AccountHolder) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateAccountHolder", ctx, ah)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// CreateAccountHolder indicates an expected call of CreateAccountHolder.
+func (mr *MockAccountStoreMockRecorder) CreateAccountHolder(ctx, ah interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateAccountHolder", reflect.TypeOf((*MockAccountStore)(nil).CreateAccountHolder), ctx, ah)
+}
+
+// CreateEphemeralAccount mocks base method.
+func (m *MockAccountStore) CreateEphemeralAccount(ctx context.Context, ea EphemeralAccount) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "CreateEphemeralAccount", ctx, ea)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// CreateEphemeralAccount indicates an expected call of CreateEphemeralAccount.
+func (mr *MockAccountStoreMockRecorder) CreateEphemeralAccount(ctx, ea interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateEphemeralAccount", reflect.TypeOf((*MockAccountStore)(nil).CreateEphemeralAccount), ctx, ea)
+}
+
+// FindDormantAccount mocks base method.
+func (m *MockAccountStore) FindDormantAccount(ctx context.Context) (*Account, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FindDormantAccount", ctx)
+	ret0, _ := ret[0].(*Account)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FindDormantAccount indicates an expected call of FindDormantAccount.
+func (mr *MockAccountStoreMockRecorder) FindDormantAccount(ctx interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindDormantAccount", reflect.TypeOf((*MockAccountStore)(nil).FindDormantAccount), ctx)
+}
+
+// FindEphemeralAccountByAccountID mocks base method.
+func (m *MockAccountStore) FindEphemeralAccountByAccountID(ctx context.Context, accountID string) (*EphemeralAccount, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FindEphemeralAccountByAccountID", ctx, accountID)
+	ret0, _ := ret[0].(*EphemeralAccount)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FindEphemeralAccountByAccountID indicates an expected call of FindEphemeralAccountByAccountID.
+func (mr *MockAccountStoreMockRecorder) FindEphemeralAccountByAccountID(ctx, accountID interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FindEphemeralAccountByAccountID", reflect.TypeOf((*MockAccountStore)(nil).FindEphemeralAccountByAccountID), ctx, accountID)
+}
+
 // GetAccountByID mocks base method.
 func (m *MockAccountStore) GetAccountByID(ctx context.Context, id string) (*Account, error) {
 	m.ctrl.T.Helper()
@@ -85,4 +215,18 @@ func (m *MockAccountStore) GetAccountByID(ctx context.Context, id string) (*Acco
 func (mr *MockAccountStoreMockRecorder) GetAccountByID(ctx, id interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetAccountByID", reflect.TypeOf((*MockAccountStore)(nil).GetAccountByID), ctx, id)
+}
+
+// UpdateAccount mocks base method.
+func (m *MockAccountStore) UpdateAccount(ctx context.Context, ac Account) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateAccount", ctx, ac)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateAccount indicates an expected call of UpdateAccount.
+func (mr *MockAccountStoreMockRecorder) UpdateAccount(ctx, ac interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateAccount", reflect.TypeOf((*MockAccountStore)(nil).UpdateAccount), ctx, ac)
 }
